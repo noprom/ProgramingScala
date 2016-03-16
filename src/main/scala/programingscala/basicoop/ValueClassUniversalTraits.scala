effe7e9e02aaa24a9254ec60d4c7b65068361878
noprom <noprom@nopromdeMacBook-Pro.local>
2016-03-16 13:52:26 +0800

    implement traits on class

---
+++ src/main/scala/programingscala/basicoop/ValueClassUniversalTraits.scala
@@ -0,0 +1,36 @@
+package programingscala.basicoop
+
+/**
+  * trait 相当于java里面的接口
+  *
+  * Author: Noprom <tyee.noprom@qq.com>
+  * Date: 16/3/16 下午1:49.
+  */
+
+trait Digitizer extends Any {
+  def digits(s: String): String = s.replaceAll("""\D""", "") // 数字化
+}
+
+trait Formatter extends Any {
+  // 格式化
+  def format(areaCode: String, exchange: String, subnumber: String): String =
+    s"($areaCode) $exchange-$subnumber"
+}
+
+class USPhoneNumber2(val s: String) extends AnyVal with Digitizer with Formatter {
+  override def toString = {
+    val digs = digits(s)
+    val areaCode = digs.substring(0, 3)
+    val exchange = digs.substring(3, 6)
+    val subnumber = digs.substring(6, 10)
+    format(areaCode, exchange, subnumber) //
+  }
+}
+
+object ValueClassUniversalTraits {
+  def main(args: Array[String]) {
+    val number = new USPhoneNumber2("987-654-3210")
+    println(number)
+    // Result: number: USPhoneNumber = (987) 654-3210
+  }
+}
